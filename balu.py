5727005a440292b942d8b569ca342d44f6d5dc77
balulomati <komati.balu@gmail.com>
2021-05-12 21:28:56 +0530

    python file

---
+++ balu.py
@@ -0,0 +1,15 @@
+a=[10,20,30,40,50]
+print(type(a))
+print(a)
+b=(10,20,30,40)
+print(type(a))
+print(b)
+c={'name':'balu','age':30}
+print(type(c))
+print(c)
+d="balu"
+print(type(d))
+print(d)
+e=10.5
+print(type(e))
+print(e)
